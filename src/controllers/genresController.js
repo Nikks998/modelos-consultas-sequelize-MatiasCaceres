668db0749181b11e70223c56805db1d664237753
Caceres Matias <kcerescmh98@gmail.com>
2023-09-22 00:28:30 -0300

    feat: modelos de movies y generos agregado

---
+++ src/controllers/genresController.js
@@ -0,0 +1,23 @@
+const { Genre } = require("../database/models");
+
+module.exports = {
+    list: (req, res) => {
+        Genre.findAll().then((genres) => {
+            return res.render("genresList", {
+                genres,
+            });
+        });
+    },
+
+    detail: (req, res) => {
+        const { id } = req.params;
+
+        Genre.findByPk(id)
+            .then((genre) => {
+                return res.render("genresDetail", {
+                    genre,
+                });
+            })
+            .catch((error) => console.log(error));
+    },
+};
